e7f8d1c405945625c6cb254692cd1a4af632045a
Toby Inkster <mail@tobyinkster.co.uk>
2013-05-17 00:11:25 +0100

    update copyright years; add disclaimer
    --HG--
    branch : RDF-iCalendar

--- lib/RDF/iCalendar/Entity.pm
+++ lib/RDF/iCalendar/Entity.pm
@@ -29,8 +29,13 @@ Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
 
 =head1 COPYRIGHT
 
-Copyright 2011 Toby Inkster
+Copyright 2011, 2013 Toby Inkster
 
-This library is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
